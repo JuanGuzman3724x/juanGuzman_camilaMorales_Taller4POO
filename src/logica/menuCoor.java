1a75810f2d3e8c160c57a651e160d5f2fe39f1a9
camilaMoralesA <camila.morales03@alumnos.ucn.cl>
2025-12-11 22:09:47 -0300

    cree la clase menu coordinador y agregue lo de modificar certificados

--- src/logica/menuCoor.java
+++ src/logica/menuCoor.java
@@ -7,13 +7,27 @@ import dominio.AsignaturaCertificacion;
 import dominio.Certificacion;
 import dominio.Cursos;
 import dominio.Estudiantes;
+import dominio.Notas;
 
 public class menuCoor {
 	  private ArrayList<Certificacion> certificaciones;
 	  private ArrayList<AsignaturaCertificacion> asignaturas;
 	  private ArrayList<Cursos> cursos;
 	  private ArrayList<Estudiantes> estudiantes;
-	public boolean modificarLinea() {
-		for(Certificacion : )
+	  private ArrayList<Notas> nota;
+
+	public boolean modificarLinea(String id, String nombre, String descripcion, int requisito, int validez) {
+		for(Certificacion c:  certificaciones) {
+			if(c.getId().equalsIgnoreCase(id)) {
+				c.setNombre(nombre);
+				c.setDescripcion(descripcion);
+				c.setRequisito(requisito);
+				c.setValidez(validez);
+				return true;
+
+			}
+		}
+		return false;
 	}
+	
 }
