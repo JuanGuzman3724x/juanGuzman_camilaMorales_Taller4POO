cfc17a62a96140dc5867456358099cb1b0d45d9f
camilaMoralesA <camila.morales03@alumnos.ucn.cl>
2025-12-11 21:22:18 -0300

    agregar set de certificacion y cree la clase menu coord

---
+++ src/logica/menuCoor.java
@@ -0,0 +1,19 @@
+package logica;
+
+import java.security.cert.Certificate;
+import java.util.ArrayList;
+
+import dominio.AsignaturaCertificacion;
+import dominio.Certificacion;
+import dominio.Cursos;
+import dominio.Estudiantes;
+
+public class menuCoor {
+	  private ArrayList<Certificacion> certificaciones;
+	  private ArrayList<AsignaturaCertificacion> asignaturas;
+	  private ArrayList<Cursos> cursos;
+	  private ArrayList<Estudiantes> estudiantes;
+	public boolean modificarLinea() {
+		for(Certificacion : )
+	}
+}
